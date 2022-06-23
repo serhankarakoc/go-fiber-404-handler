f965d36e82277156ac127b1cbadb29126fa84caf
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-06-23 14:18:03 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,32 @@
+// ⚡️ Fiber is an Express inspired web framework written in Go with ☕️
+// 🤖 Github Repository: https://github.com/gofiber/fiber
+// 📌 API Documentation: https://docs.gofiber.io
+
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Fiber instance
+	app := fiber.New()
+
+	// Routes
+	app.Get("/hello", hello)
+
+	// 404 Handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("I made a ☕ for you!")
+}
